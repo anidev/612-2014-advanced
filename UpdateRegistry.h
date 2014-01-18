7baf2317d46799e0681f1be874b546f68f512ba6
swarage <sdcoolio@gmail.com>
2014-01-17 20:02:04 -0500

    testing data logging and update registry

---
+++ UpdateRegistry.h
@@ -0,0 +1,24 @@
+#ifndef UPDATE_REGISTRY
+#define UPDATE_REGISTRY
+
+#include <vector>
+
+typedef void* obj;
+typedef void(*helper)(obj);
+
+struct registry_object {
+    obj o;
+    helper h;
+};
+
+class UpdateRegistry {
+public:
+    UpdateRegistry();
+    ~UpdateRegistry();
+    void add(registry_object);
+    void update();
+private:
+    std::vector<registry_object> registry;
+};
+
+#endif
