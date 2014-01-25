5c3adf878e403e88aba28d15084e8c7c844a4ca9
swarage <sdcoolio@gmail.com>
2014-01-25 12:14:40 -0500

    controls

--- Controls.h
+++ Controls.h
@@ -12,4 +12,26 @@ enum Trigger {
 
 static const Axis TRIGGER_AXIS = 3; //may need to rename this later
 
+/*
+Buttons
+01: A (green/bottom)
+02: B (red/right)
+03: X (blue/left)
+04: Y (yellow/top)
+05: Left Bumper (L1)
+06: Right Bumper (R1)
+07: Back (left)
+08: Start (right)
+09: Left Joystick Click
+10: Right Joystick Click
+
+Axes
+01: Left Joystick X: -left, +right
+02: Left Joystick Y: -up, +down
+03: Triggers (L2/R2): -right, +left, 0 at rest, 0 if both pressed
+04: Right Joystick X: -left, +right
+05: Right Joystick Y: -up, +down
+06: DPad X: -left, +right
+*/ 
+
 #endif
