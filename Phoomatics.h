b61f5bd106fe5f3d62359c27b331aa7607a8352c
swarage <sdcoolio@gmail.com>
2014-01-25 13:25:18 -0500

    added registry for pneumatics

--- Phoomatics.h
+++ Phoomatics.h
@@ -3,14 +3,28 @@
 
 #include <Relay.h>
 #include <DigitalInput.h>
+#include <Timer.h>
+#include <DoubleSolenoid.h>
+
+#include <vector>
+
+struct pnumObj
+{
+    double time;
+    Timer* timer;
+    DoubleSolenoid* solenoid;
+    DoubleSolenoid::Value val;
+};
 
 class Phoomatics
 {
 public:
     Phoomatics(uint8_t switchMod,uint32_t switchPort,uint8_t compMod,uint32_t compPort);
     void pressurize();
+    void addSolenoid(pnumObj);
 private:
     DigitalInput* pnumSwitch;
     Relay* compressor;
+    std::vector<pnumObj> solenoids;
 };
 #endif
