e475febab9b228ee621290b47b3a2cf585aa7134
swarage <sdcoolio@gmail.com>
2014-01-30 13:20:49 -0500

    fixed arm's update

--- Arm.cpp
+++ Arm.cpp
@@ -13,8 +13,8 @@ Arm::Arm(uint8_t tiltDev, uint8_t grabDev, uint32_t SolMod, uint32_t SolPort)
     joy -> pushBtn(1, (void*)this, &updateArm); //A -- open
     joy -> pushBtn(2, (void*)this, &updateArm); //B -- close
     
-    joy -> pushBtn(3, (void*)this, &updateArm); // -- open
-    joy -> pushBtn(4, (void*)this, &updateArm); //B -- close
+    joy -> pushBtn(3, (void*)this, &updateArm); //X -- tilt up
+    joy -> pushBtn(4, (void*)this, &updateArm); //Y -- tilt down
     
     grabWheel = new Talon(3,4); //fake values
     tiltControl = new CANJaguar(tiltDev);
@@ -48,10 +48,21 @@ void Arm::grab()
 
 void Arm::updateArm(void* o, unsigned int btn) {
     Arm* a = (Arm*)o;
-    if(btn==2) {
+    if(btn==2)
+    {
         a->closeArm();
-    } else {
+    }
+    else if (btn==1)
+    {
         a->openArm();
+    } 
+    else if (btn==3)
+    {
+        a->tiltUp();
+    } 
+    else
+    {
+        a->tiltDown();
     }
 }
 
